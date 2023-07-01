4d94d9532c3381b1a3e8ef761467d5a39873392a
suraj pro <71000909+suraj719@users.noreply.github.com>
2023-07-01 17:32:46 +0530

    changed home ui

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -3,24 +3,49 @@ import "../index.css";
 
 const Home = () => {
   return (
-    <>
-    <div className="flex flex-wra justify-center items-center px-4 lg:px-40 py-20 lg:py-8">
-      <div className="lg:w-1/2 text-xl font-semibold">
-          <p className="font-bold text-3xl underline text-center my-5">InnerSerenity</p>
-          <p>
-              Connect with expert professionals securely and conveniently. Book
-              appointments, access resources, and find support tailored to your needs
-              all from the comfort of your home. Begin your journey towards a
-              healthier mind today.
+    <div className="">
+      <div className="flex flex-wrap justify-center items-center px-4 lg:px-40 mt-8">
+        <div className="lg:w-1/2 mx-auto ">
+          <p className="font-bold text-6xl text-green-600 text-center">
+            InnerSerenity
           </p>
-          
+          <p className="font-bold text-2xl p-6" style={{ color: "#57494b" }}>
+            Unlock the Fantom-AWS Gateway to Inner Peace with InnerSerenity
+          </p>
+          <p className="text-xl font-semibold">
+            Experience the magic of InnerSerenity, where the Fantom-AWS
+            blockchain platform meets mental health support. Connect with
+            qualified professionals and supportive peers in a secure and private
+            space. Discover transparency through feedback and ratings, making
+            informed choices for your well-being. Accessible and affordable,
+            InnerSerenity breaks barriers to mental health services.
+          </p>
+        </div>
+        <div className="w-2/5 hidden lg:block my-auto ">
+          <img src="../prof-reg.jpg" alt="hero-1" />
+        </div>
       </div>
-      <div className="w-1/2 hidden lg:block">
-          <img src="../hero.png" alt="hero" />
+      <div className="flex flex-wrap justify-center items-center px-4 lg:px-40  lg:mt-16 ">
+        <div className="lg:w-1/2 w-full sm:mb-20 my-auto">
+          <img src="../hero.png" alt="hero-2" />
+        </div>
+        <div className="lg:w-1/2 mx-auto">
+          <p className="text-xl font-semibold pt-5">
+            Enhance your journey with our unique event ticketing feature.
+            Immerse yourself in specialized events, guided by experts issuing
+            ERC1155 tokens. Embrace personal growth and enlightenment through
+            engaging experiences. InnerSerenity empowers you to find balance,
+            harmony, and inner peace.
+            <p className="mt-5">
+              Join us now and unlock the gateway to your InnerSerenity. Experience
+              the transformative potential of blockchain technology, as we create
+              a connected, resilient, and serene future together.
+            </p>
+          </p>
+        </div>
       </div>
     </div>
-  </>
-  )
+  );
 };
 
 export default Home;
